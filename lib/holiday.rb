003ce854ad60ed63f19d924756a1d50545604b88
Christerle Nounoute <christerlesn@gmail.com>
2020-09-22 19:17:27 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -49,6 +49,6 @@ def all_supplies_in_holidays(holiday_hash)
 end
 
 def all_holidays_with_bbq(holiday_hash)
-binding.pry
-  holiday_hash
+  holiday_hash.map do |season, holiday|
+    binding.pry
 end
