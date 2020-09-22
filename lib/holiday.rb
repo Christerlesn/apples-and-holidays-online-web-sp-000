d942c3ed3efc5d59bc56cc1ccfc5e93671c36cd4
Christerle Nounoute <christerlesn@gmail.com>
2020-09-22 19:31:04 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -52,7 +52,6 @@ def all_holidays_with_bbq(holiday_hash)
   holiday_hash.map do |season, holidays|
     holidays.map do |holiday, decoration|
      decoration.include?("BBQ") ? holiday : nil
-
     end
   end.flatten
 end
