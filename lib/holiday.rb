a71e1bcde28eae9325e4f27df1b738b5691dd06d
Christerle Nounoute <christerlesn@gmail.com>
2020-09-22 18:45:11 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -50,10 +50,5 @@ def all_supplies_in_holidays(holiday_hash)
 end
 
 def all_holidays_with_bbq(holiday_hash)
-holiday_hash.each do |season, holiday|
-  if season.include?("BBQ")
-    holiday.keys
-  end
-end
-end
+
 end
