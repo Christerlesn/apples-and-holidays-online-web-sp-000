67e21165a1df574d6642fd9753396358bfd381c0
Christerle Nounoute <christerlesn@gmail.com>
2020-09-22 19:05:40 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -36,8 +36,7 @@ def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_arr
 end
 
 def all_winter_holiday_supplies(holiday_hash)
-winter_holidays = holiday_hash[:winter].values
-winter_holidays.flatten
+holiday_hash[:winter].values.flatten
 end
 
 def all_supplies_in_holidays(holiday_hash)
